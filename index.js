67dd3b48d329a164b33fd1d3585aa51433e79689
xiaomin zhu <xiaomin.zhu88@gmail.com>
2020-05-08 19:45:23 +0200

    Add public dir. and html file Create server

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const publicFile = require('./public/publicFile'); // 'import' the created file in 'public' dir
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // use fs to read file which i created inside 'public' dir
+  fs.readFile('./public/public.html', (err, data) => {
+    if (err) {
+      res.setHeader('Content-type', 'text/plain');
+
+      // error 404!
+      res.statusCode = 404;
+      res.end(err);
+    } else {
+      //set type of the content on localhost page with hostname and port, could also change
+      //to text/html then it shows html -Header
+      res.setHeader('Content-type', 'text/html');
+
+      // port 200 OK!
+      res.statusCode = 200;
+
+      // finish res
+      res.end(data);
+    }
+  });
+});
+
+//  show server console with run on localhost
+server.listen(publicFile.port, publicFile.hostname, () => {
+  console.log(
+    `Server running at http://${publicFile.hostname}:${publicFile.port}/`,
+  );
+});
---
+++ public/publicFile.js
@@ -0,0 +1,6 @@
+const publicFile = {
+  hostname: '127.0.0.1',
+  port: 3000,
+};
+
+module.exports = publicFile;  //'export' module
